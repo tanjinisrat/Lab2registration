b5f2a0950c89084166927ae2ce0a3658781708b4
tanjinisrat <75239074+tanjinisrat@users.noreply.github.com>
2021-02-05 21:10:11 +0600

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Exersice</title>
+</head>
+<body>
+<h1>Registration</h1>
+
+
+
+<br><br>
+<from>
+<table>
+<tr>
+<td>Name:</td>
+<td><input type="text" id="f" name="fname"></td></tr>
+<tr>
+<td>User Name</td>
+<td><input type="text" id="l" name="lname"></td></tr>
+<tr>
+<td>Email</td>
+<td><input type="text" id="m" name="email"></td></tr>
+<td>Gander </td>
+<td><input type="radio" id="male" name="gander" value="male">Male</td></tr>
+<td><input type="radio" id="female" name="gander" value="female">Female</td></tr>
+<td><input type="radio" id="other" name="gander" value="Othe">Other</td></tr>
+<tr>
+<td>Password</td>
+<td><input type="password" value="password"></td></tr>
+<td>Conform Password</td>
+<td><input type="password" value="password"></td></tr>
+<tr>
+<td>Date of Birth</td>
+<td><input type="date"></td></tr>
+<td><input type="submit"  value="Next">
+<input type="reset" value="reset"></td></tr>
+
+</table>
+
+</from>
+
+</body>
+
+</html>
